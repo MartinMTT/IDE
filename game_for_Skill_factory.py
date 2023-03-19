facc58454d7602e474d08ed4c43812bb32d6a919
MartinMTT <botafogott@gmx.com>
2023-03-19 11:54:13 +0100

    Skill hotovo

--- game_for_Skill_factory.py
+++ game_for_Skill_factory.py
@@ -1,26 +1,29 @@
 import numpy as np
 
-def gen_1_100int(men,viac):                                 # generuje polovicu medzi menej a viac
-    return (men+viac)//2                
+def gen_half_int(men,viac):                                 # generuje polovicu medzi menej a viac
 
-def half_interval(number:int=1) -> int :                   # zisti na kolkokrat najde
+    return (men+viac)//2        
+
+def half_interval(number:int=1) -> int :                   # zisti na kolkokrat najde number
                                                             # half interval alghoritmus    
     count=0
-    predict_number=gen_1_100int(1,101)
+    predict_number=gen_half_int(1,100)
+#    print(number,count,predict_number)                      # aby clovek videl
     mensie=1
     vacsie=100
 
     while True :
         count+=1                                            # pocita pokusy
-#        print(number,count,predict_number)                 # aby clovek videl/man to see
         if predict_number==number:                          # algoritmus zmensenia intervalu
             break
         elif predict_number < number:
             mensie=predict_number                           # zmensi interval
-            predict_number=gen_1_100int(mensie,vacsie)
+            predict_number=gen_half_int(mensie,vacsie)
+#            print(number,count,predict_number)             # man to see
         elif predict_number > number:
             vacsie=predict_number
-            predict_number=gen_1_100int(mensie,vacsie)
+            predict_number=gen_half_int(mensie,vacsie)
+#            print(number,count,predict_number)
             
     return count
 
@@ -30,7 +33,7 @@ def score_game(half_interval) -> int:                      # rata priemer hladan
     count_ls = [] 
 #    np.random.seed(11)                                     # neviem naco to chcu
                                                             # /why? it false output
-    random_array = np.random.randint(1, 101, size=(1))    # generuje zoznam 100 nahodnych cisel
+    random_array = np.random.randint(1, 100, size=(1000))    # generuje zoznam 1000 nahodnych cisel
     
     pocitadlo=0
     for num in random_array:                                
